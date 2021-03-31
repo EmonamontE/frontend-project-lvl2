f0511cc6cf9cc92e6f6fe8827670ff1ef388e7d9
emonamonte <racoonsmr@gmail.com>
2021-03-31 19:40:30 +0400

    Step 8.3

--- src/buildingTree.js
+++ src/buildingTree.js
@@ -1,25 +1,27 @@
 import _ from 'lodash';
 
 const buildingTree = (data1, data2) => {
-  const keys = _.union(Object.keys(data1), Object.keys(data2));
-  const newKeys = keys.slice().sort();
-  return newKeys.map((currentKey) => {
-    if (!_.has(data1, currentKey)) {
-      return { key: currentKey, value: data2[currentKey], type: 'added' };
-    }
-    if (!_.has(data2, currentKey)) {
-      return { key: currentKey, value: data1[currentKey], type: 'removed' };
-    }
-    if (_.isObject(data1[currentKey]) && _.isObject(data2[currentKey])) {
-      return { key: currentKey, children: buildingTree(data1[currentKey], data2[currentKey]), type: 'nested' };
-    }
-    if (data1[currentKey] !== data2[currentKey]) {
-      return {
-        key: currentKey, oldValue: data1[currentKey], newValue: data2[currentKey], type: 'changed',
-      };
-    }
-    return { key: currentKey, value: data1[currentKey], type: 'unchanged' };
-  });
+  const keys1 = Object.keys(data1);
+  const keys2 = Object.keys(data2);
+  return _.union(keys1, keys2)
+    .sort()
+    .map((currentKey) => {
+      if (!_.has(data1, currentKey)) {
+        return { key: currentKey, value: data2[currentKey], type: 'added' };
+      }
+      if (!_.has(data2, currentKey)) {
+        return { key: currentKey, value: data1[currentKey], type: 'removed' };
+      }
+      if (_.isObject(data1[currentKey]) && _.isObject(data2[currentKey])) {
+        return { key: currentKey, children: buildingTree(data1[currentKey], data2[currentKey]), type: 'nested' };
+      }
+      if (data1[currentKey] !== data2[currentKey]) {
+        return {
+          key: currentKey, oldValue: data1[currentKey], newValue: data2[currentKey], type: 'changed',
+        };
+      }
+      return { key: currentKey, value: data1[currentKey], type: 'unchanged' };
+    });
 };
 
 export default buildingTree;
